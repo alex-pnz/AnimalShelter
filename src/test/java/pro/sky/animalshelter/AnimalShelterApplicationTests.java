f4d882ee718f94911c4e33434371d6ac5031a0f2
Sasha <alex_penza@list.ru>
2023-08-24 13:06:07 +0300

    Animal Shelter Team Work

---
+++ src/test/java/pro/sky/animalshelter/AnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.animalshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
