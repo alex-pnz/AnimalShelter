a4960767f592b368591f30a9f576203d20cf55e2
Sasha <alex_penza@list.ru>
2023-09-03 13:22:53 +0300

    main menu & security Info

--- src/main/java/pro/sky/animalshelter/listener/TelegramBotUpdateListener.java
+++ src/main/java/pro/sky/animalshelter/listener/TelegramBotUpdateListener.java
@@ -2,6 +2,7 @@ package pro.sky.animalshelter.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.CallbackQuery;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
@@ -10,6 +11,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.animalshelter.service.MenuService;
+import pro.sky.animalshelter.service.MessageService;
 
 import java.util.List;
 
@@ -20,6 +23,12 @@ public class TelegramBotUpdateListener implements UpdatesListener {
     @Autowired
     private TelegramBot bot;
 
+    @Autowired
+    private MenuService menuService;
+
+    @Autowired
+    private MessageService messageService;
+
     @PostConstruct
     public void init() {
         bot.setUpdatesListener(this);
@@ -29,27 +38,49 @@ public class TelegramBotUpdateListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             // пишем обработчики в виде функций void functionName(Update update), вызываем здесь
-            String command = update.message().text();
-            switch (command) {
-                case "/start" -> {
-                    //
+
+            if (update.message() != null) { // Меню InlineKeyboard не передает message, поэтому ловим  callback который передаем в callbackData
+                Long chatId = update.message().chat().id();
+                String command = update.message().text();
+                switch (command) {
+                    case "/start" -> {
+                        menuService.showMainMenu(chatId);
+                    }
+                    case "/about" -> {
+                        //
+                    }
+                    case "/schedule" -> {
+                        //
+                    }
+                    case "/security" -> {
+                        messageService.showSecurityInfo(chatId);
+                    }
+                    case "/safety" -> {
+                        //
+                    }
+                    default -> {
+                        defaultHandler(update);
+                    }
                 }
-                case "/about" -> {
-                    //
+            } else if (update.callbackQuery() != null) {   // Здесь обрабатываем callback полученный из меню, потом надо добавить другие кейсы из других меню которые сделаем позже
+                CallbackQuery callbackQuery = update.callbackQuery();
+                String callback = callbackQuery.data();
+                SendMessage message = null;
+                switch (callback) {
+                    case "cat" -> {
+                        message = new SendMessage(update.callbackQuery().message().chat().id(), "Test: Попадаем в раздел кошки"); // Для тестирования, потом заменить
+                    }
+                    case "dog" -> {
+                        message = new SendMessage(update.callbackQuery().message().chat().id(),"Test: Попадаем в раздел собаки");//Для тестирования, потом заменить
+                    }
+                    default -> {
+                        defaultHandler(update);
+                    }
                 }
-                case "/schedule" -> {
-                    //
-                }
-                case "/security" -> {
-                    //
-                }
-                case "/safety" -> {
-                    //
-                }
-                default -> {
-                    defaultHandler(update);
-                }
+
+                bot.execute(message);
             }
+
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
