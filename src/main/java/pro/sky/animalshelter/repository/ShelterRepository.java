e3b26427071a9f1d4a5ab3c39f669d7bf7e45670
Gleb Lykov <glebdom@gmail.com>
2023-09-16 21:43:06 +0300

    Saving visit and receiving type of visited shelter are implemented

--- src/main/java/pro/sky/animalshelter/repository/ShelterRepository.java
+++ src/main/java/pro/sky/animalshelter/repository/ShelterRepository.java
@@ -1,10 +1,17 @@
 package pro.sky.animalshelter.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import pro.sky.animalshelter.model.AnimalType;
 import pro.sky.animalshelter.model.Shelter;
+
+import java.util.List;
 
 /**
  * Класс, хранящий данные о приюте
  */
 public interface ShelterRepository extends JpaRepository<Shelter, Long> {
+
+    @Query(value = "select * from shelters where shelter_type like ?1", nativeQuery = true)
+    List<Shelter> findByShelterType(String type);
 }
--- src/main/java/pro/sky/animalshelter/repository/VisitRepository.java
+++ src/main/java/pro/sky/animalshelter/repository/VisitRepository.java
@@ -1,13 +1,19 @@
 package pro.sky.animalshelter.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 import pro.sky.animalshelter.model.Visit;
 import pro.sky.animalshelter.model.Visitor;
+
+import java.util.List;
 
 /**
  * Класс, хранящий данные о визите посетителя в приют
  */
 public interface VisitRepository extends JpaRepository<Visit, Long> {
-    Visit findByVisitor(Visitor visitor);
+    @Query(value = "select * from visitors_per_shelter where visitor_id = ?1 order by id desc limit 1", nativeQuery = true)
+    Visit findByVisitor(Long visitorId);
+
+    List<Visit> findAllById(Long id);
 }
