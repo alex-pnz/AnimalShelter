35713e1cc9f36b16b3583cdc47afa5fd30d67cd8
Alex <shurik228154@mail.ru>
2023-09-28 13:16:11 +0300

    corrections

--- src/main/java/pro/sky/animalshelter/service/VolunteerService.java
+++ src/main/java/pro/sky/animalshelter/service/VolunteerService.java
@@ -2,6 +2,7 @@ package pro.sky.animalshelter.service;
 
 import org.springframework.stereotype.Service;
 import pro.sky.animalshelter.model.Volunteer;
+import pro.sky.animalshelter.model.enums.Action;
 import pro.sky.animalshelter.repository.VolunteerRepository;
 
 /**
@@ -31,21 +32,45 @@ public class VolunteerService {
         volunteerRepository.deleteById(id);
     }
 
+    /**
+     * Изменяет статус волонтера
+     *
+     * @param chatId
+     * @param state
+     */
     public void setVolunteerFree(Long chatId, boolean state) {
         Volunteer volunteer = volunteerRepository.findByChatId(chatId);
         volunteer.setFree(state);
         volunteerRepository.save(volunteer);
     }
 
+    /**
+     * Проверяет является ли пользователь волонтером
+     *
+     * @param chatId
+     */
     public boolean isVolunteer(Long chatId) {
         Volunteer volunteer = volunteerRepository.findByChatId(chatId);
         return volunteer != null;
     }
-    public void saveAction(Long chatId, String action) {
+
+    /**
+     * Сохраняет действие волонтера
+     *
+     * @param chatId
+     * @param action
+     */
+    public void saveAction(Long chatId, Action action) {
         Volunteer volunteer = volunteerRepository.findByChatId(chatId);
         volunteer.setAction(action);
         volunteerRepository.save(volunteer);
     }
+
+    /**
+     * Проверяет выполняет ли волонтер действие в данный момент
+     *
+     * @param chatId
+     */
     public boolean isAction(Long chatId){
         Volunteer volunteer = volunteerRepository.findByChatId(chatId);
         return volunteer.getAction() != null;
